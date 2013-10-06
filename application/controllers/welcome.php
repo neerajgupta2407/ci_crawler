508d769e7f150ea3e4483c0e614e3987d6b2e0c7
neerajgupta2407 <neerajgupta2407@gmail.com>
2013-10-06 15:04:41 +0530

    initial commit

---
+++ application/controllers/welcome.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->library('crawler');
+		$this->crawler->set_url('http://www.snapdeal.com/product/red-riding-brown-sky-blue/457016');
+		//$output=$this->crawler->get_title();
+				//var_dump($output);
+		//$output=$this->crawler->get_keywords();
+		
+		//var_dump($output);
+		//$output=$this->crawler->get_text();
+		
+		$arr=$this->crawler->get_tag('h1[itemprop=name]');
+		//echo 'tag is ';
+		var_dump($arr);
+		$arr2=$this->crawler->get_tag('span[id=selling-price-id]');
+		
+		var_dump($arr2);//var_dump($output);
+		
+		
+		//$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
